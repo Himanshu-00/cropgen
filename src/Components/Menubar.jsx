e3ad4c6d9527cd6b9125f1b9b58cac86abc1bfcf
Himanshu-00 <himanshuvinchurkar1387@gmail.com>
2024-06-02 11:50:45 +0530

    C

--- src/Components/Menubar.jsx
+++ src/Components/Menubar.jsx
@@ -61,7 +61,7 @@ const Menubar = ({ selectedMenu, setSelectedMenu }) => {
         case 'CropAnalytics':
           return (
             <Card title="Crop Analytics" style={{ width: '100%', height: '100%' }}>
-              <MyComponent />
+              <MyComponent/>
             </Card>
           );
         // Cases for other menu items
